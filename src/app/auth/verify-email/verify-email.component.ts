4118e7e336f9bd66f2568a431b8b0ae91973bd49
mnahsanofficial <mnahsanofficial@gmail.com>
2024-07-10 22:53:05 +0600

    verify email page done

--- src/app/auth/verify-email/verify-email.component.ts
+++ src/app/auth/verify-email/verify-email.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/shared/services/auth.service';
 
 @Component({
   selector: 'app-verify-email',
   templateUrl: './verify-email.component.html',
   styleUrls: ['./verify-email.component.css']
 })
-export class VerifyEmailComponent {
+export class VerifyEmailComponent implements OnInit{
+
+  loading:boolean = false;
+  email = "test@gamil.com";
+  constructor(public authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+  resendEmail(){
+
+  }
 
 }
