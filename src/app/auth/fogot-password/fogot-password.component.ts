5a4b09d6c5ae5f58dfc5c9ccee2d089e625d2e72
mnahsanofficial <mnahsanofficial@gmail.com>
2024-07-10 22:39:58 +0600

    forget password page done

--- src/app/auth/fogot-password/fogot-password.component.ts
+++ src/app/auth/fogot-password/fogot-password.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/shared/services/auth.service';
 
 @Component({
   selector: 'app-fogot-password',
   templateUrl: './fogot-password.component.html',
   styleUrls: ['./fogot-password.component.css']
 })
-export class FogotPasswordComponent {
+export class FogotPasswordComponent implements OnInit{
+
+  loading: boolean = false;
+  constructor(public authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+  reset(){
+
+  }
 
 }
